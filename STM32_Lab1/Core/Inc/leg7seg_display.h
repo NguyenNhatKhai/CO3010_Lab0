721348cb632f9e2f75985882f5aacec36f191b49
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-19 22:35:50 +0700

    init # 10:35PM 19/09/2023

---
+++ STM32_Lab1/Core/Inc/leg7seg_display.h
@@ -0,0 +1,13 @@
+/*
+ * leg7seg_display.h
+ *
+ *  Created on: Sep 19, 2023
+ *      Author: Nhat Khai
+ */
+
+#ifndef INC_LEG7SEG_DISPLAY_H_
+#define INC_LEG7SEG_DISPLAY_H_
+
+
+
+#endif /* INC_LEG7SEG_DISPLAY_H_ */
---
+++ STM32_Lab1/Core/Src/leg7seg_display.c
@@ -0,0 +1,8 @@
+/*
+ * leg7seg_display.c
+ *
+ *  Created on: Sep 19, 2023
+ *      Author: Nhat Khai
+ */
+
+#include "leg7seg_display.h"
