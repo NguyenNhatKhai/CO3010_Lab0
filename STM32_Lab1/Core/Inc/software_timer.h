6677d294efaca998d7ea08e1748b16d1d3e0e468
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-19 15:53:32 +0700

    update Exercise1

---
+++ STM32_Lab1/Core/Inc/software_timer.h
@@ -0,0 +1,17 @@
+/*
+ * software_timer.h
+ *
+ *  Created on: Sep 19, 2023
+ *      Author: Nhat Khai
+ */
+
+#ifndef INC_SOFTWARE_TIMER_H_
+#define INC_SOFTWARE_TIMER_H_
+
+extern int timer0_flag;
+
+void setTimer0(int duration);
+
+void runTimer();
+
+#endif /* INC_SOFTWARE_TIMER_H_ */
