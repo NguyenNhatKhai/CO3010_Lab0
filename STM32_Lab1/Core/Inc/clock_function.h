45dc721c8f5209d92c94ce4a2a066d9d25a98757
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-19 23:47:49 +0700

    update Exercise6 #11:50PM 19/09/2023

---
+++ STM32_Lab1/Core/Inc/clock_function.h
@@ -0,0 +1,16 @@
+/*
+ * clock_function.h
+ *
+ *  Created on: Sep 19, 2023
+ *      Author: Nhat Khai
+ */
+
+#ifndef INC_CLOCK_FUNCTION_H_
+#define INC_CLOCK_FUNCTION_H_
+
+#include "main.h"
+#include "software_timer.h"
+
+void testClock(int duration);
+
+#endif /* INC_CLOCK_FUNCTION_H_ */
