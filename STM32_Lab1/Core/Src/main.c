c2be1e6137010cde983d246d6971ab0ff3a63f1d
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-19 21:20:17 +0700

    init Exercise4 # 21:20 19/09/2023

--- STM32_Lab1/Core/Src/main.c
+++ STM32_Lab1/Core/Src/main.c
@@ -195,10 +195,15 @@ static void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOA_CLK_ENABLE();
+  __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(GPIOA, LED_0R_Pin|LED_0Y_Pin|LED_0G_Pin|LED_1R_Pin
                           |LED_1Y_Pin|LED_1G_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, SEG_0A_Pin|SEG_0B_Pin|SEG_0C_Pin|SEG_0D_Pin
+                          |SEG_0E_Pin|SEG_0F_Pin|SEG_0G_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pins : LED_0R_Pin LED_0Y_Pin LED_0G_Pin LED_1R_Pin
                            LED_1Y_Pin LED_1G_Pin */
@@ -208,6 +213,15 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOA, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SEG_0A_Pin SEG_0B_Pin SEG_0C_Pin SEG_0D_Pin
+                           SEG_0E_Pin SEG_0F_Pin SEG_0G_Pin */
+  GPIO_InitStruct.Pin = SEG_0A_Pin|SEG_0B_Pin|SEG_0C_Pin|SEG_0D_Pin
+                          |SEG_0E_Pin|SEG_0F_Pin|SEG_0G_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 }
 
