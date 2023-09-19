7acb4635a97584132381f01fd271da09cc8ad058
NhatKhai352528 <nhatkhai352528@gmail.com>
2023-09-19 21:22:29 +0700

    update Exercise1 # 21:22 19/09/2023

--- STM32_Lab1/Core/Src/software_timer.c
+++ STM32_Lab1/Core/Src/software_timer.c
@@ -8,12 +8,13 @@
 #include "software_timer.h"
 
 int timer0_counter = 0;
-int timer0_flag = 0;
+timer0_flag = 0;
 
 void setTimer0(int duration){
 	timer0_counter = duration;
 	timer0_flag = 0;
 }
+
 void runTimer(){
 	if(timer0_counter > 0){
 		timer0_counter --;
